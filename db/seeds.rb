b89075a1050bdd2757a8d28002586755bb975d0d
lexaliemu <lexaliemu@gmail.com>
2018-11-27 12:31:54 +0100

    add user_id field on the seed and db:seed it

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,10 @@ movies = Movie.create([
     description: "After a wealthy banker is given an opportunity to participate in a mysterious game, his life is turned upside down when he becomes unable to distinguish between the game and reality.",
     synopsis: "Nicholas Van Orton is a very wealthy San Francisco banker, but he is an absolute loner, even spending his birthday alone. In the year of his 48th birthday (the age his father committed suicide) his brother Conrad, who has gone long ago and surrendered to addictions of all kinds, suddenly returns and gives Nicholas a card giving him entry to unusual entertainment provided by something called Consumer Recreation Services (CRS). Giving in to curiosity, Nicholas visits CRS and all kinds of weird and bad things start to happen to him.",
     director: "David Fincher",
-    cover:"e4dqfucife0mv6m5agag",
-    price: 4.99,
-    rating: 7.8
+    cover: "e4dqfucife0mv6m5agag",
+    price: 3.99,
+    rating: 7.8,
+    user_id: 1
   },
   {
     title: "Boogie Nights",
@@ -26,8 +27,9 @@ movies = Movie.create([
     synopsis: "Adult film director Jack Horner is always on the lookout for new talent and it’s only by chance that he meets Eddie Adams who is working as a busboy in a restaurant. Eddie is young, good looking and plenty of libido to spare. Using the screen name Dirk Diggler, he quickly rises to the top of his industry winning awards year after year. Drugs and ego however come between Dirk and those around him and he soon finds that fame is fleeting.",
     director: "Paul Thomas Anderson",
     cover: "avny0dxavadjauwhufgc",
-    price: 4.99,
-    rating: 7.9
+    price: 3.99,
+    rating: 7.9,
+    user_id: 1
   },
   {
     title: "Les Invasions Barbares",
@@ -37,7 +39,8 @@ movies = Movie.create([
     synopsis: "In this belated sequel to ‘The Decline of the American Empire’, 50-something Montreal college professor, Remy, learns that he is dying of liver cancer. He decides to make amends meet to his friends and family before he dies. He first tries to made peace with his ex-wife Louise, who asks their estranged son Sebastian, a successful businessman living in London, to come home. Sebastian makes the impossible happen, using his contacts and disrupting the entire Canadian system in every way possible to help his father fight his terminal illness to the bitter end, while he also tries to reunite his former friends, Pierre, Alain, Dominique, Diane, and Claude to see their old friend before he passes on.",
     director: "Denys Arcand",
     cover: "a5kvvuts95mt1rjggbtc",
-    price: 4.99,
-    rating: 7.7
+    price: 3.99,
+    rating: 7.7,
+    user_id: 2
   }
 ])
