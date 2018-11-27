2951f8337feebaf5f18d03d58b748e0c3ff7010d
william segard <william.segard@gmail.com>
2018-11-27 15:00:14 +0100

    good seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
-movies = Movie.create([
+puts 'Deleting all movies'
+Movie.destroy_all
+
+puts 'Seeding movies without cover'
+movies = []
+movies << Movie.new(
   {
     title: "The Game",
     release_date: 1997,
@@ -6,11 +11,12 @@ movies = Movie.create([
     description: "After a wealthy banker is given an opportunity to participate in a mysterious game, his life is turned upside down when he becomes unable to distinguish between the game and reality.",
     synopsis: "Nicholas Van Orton is a very wealthy San Francisco banker, but he is an absolute loner, even spending his birthday alone. In the year of his 48th birthday (the age his father committed suicide) his brother Conrad, who has gone long ago and surrendered to addictions of all kinds, suddenly returns and gives Nicholas a card giving him entry to unusual entertainment provided by something called Consumer Recreation Services (CRS). Giving in to curiosity, Nicholas visits CRS and all kinds of weird and bad things start to happen to him.",
     director: "David Fincher",
-    cover: "e4dqfucife0mv6m5agag",
     price: 5.99,
     rating: 7.8,
     user_id: 1
-  },
+  })
+
+movies << Movie.new(
   {
     title: "Boogie Nights",
     release_date: 1997,
@@ -18,11 +24,12 @@ movies = Movie.create([
     description: "The story of a young man's adventures in the California pornography industry of the late 1970s and early 1980s.",
     synopsis: "Adult film director Jack Horner is always on the lookout for new talent and it’s only by chance that he meets Eddie Adams who is working as a busboy in a restaurant. Eddie is young, good looking and plenty of libido to spare. Using the screen name Dirk Diggler, he quickly rises to the top of his industry winning awards year after year. Drugs and ego however come between Dirk and those around him and he soon finds that fame is fleeting.",
     director: "Paul Thomas Anderson",
-    cover: "avny0dxavadjauwhufgc",
     price: 5.99,
     rating: 7.9,
     user_id: 1
-  },
+  })
+
+movies << Movie.new(
   {
     title: "Les Invasions Barbares",
     release_date: 2003,
@@ -30,9 +37,18 @@ movies = Movie.create([
     description: "During his final days, a dying man is reunited with old friends, former lovers, his ex-wife, and his estranged son.",
     synopsis: "In this belated sequel to ‘The Decline of the American Empire’, 50-something Montreal college professor, Remy, learns that he is dying of liver cancer. He decides to make amends meet to his friends and family before he dies. He first tries to made peace with his ex-wife Louise, who asks their estranged son Sebastian, a successful businessman living in London, to come home. Sebastian makes the impossible happen, using his contacts and disrupting the entire Canadian system in every way possible to help his father fight his terminal illness to the bitter end, while he also tries to reunite his former friends, Pierre, Alain, Dominique, Diane, and Claude to see their old friend before he passes on.",
     director: "Denys Arcand",
-    cover: "a5kvvuts95mt1rjggbtc",
     price: 5.99,
     rating: 7.7,
-    user_id: 2
-  }
-])
+    user_id: 1
+  })
+
+
+cover_urls = ["https://res.cloudinary.com/dvtjorjay/image/upload/v1543316511/e4dqfucife0mv6m5agag.jpg",
+                "https://res.cloudinary.com/dvtjorjay/image/upload/v1543316383/avny0dxavadjauwhufgc.jpg",
+                "https://res.cloudinary.com/dvtjorjay/image/upload/v1543316528/a5kvvuts95mt1rjggbtc.jpg"]
+
+puts 'Adding cover and saving'
+movies.each_with_index do |movie, index|
+  movies[index].remote_cover_url = cover_urls[index]
+  movie.save
+end
