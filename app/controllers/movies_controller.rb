898c7919fedf12938ceff55d4e30c23e73f52ff6
lexaliemu <lexaliemu@gmail.com>
2018-11-29 10:58:56 +0100

    first dev on the search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,16 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
-    @user = current_user
+    if params[:query].present?
+      sql_query = " \
+        movies.title @@ :query \
+        OR movies.syllabus @@ :query \
+        OR directors.first_name @@ :query \
+        OR directors.last_name @@ :query \
+      "
+      @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @movies = Movie.all
+    end
   end
 
   def show
